Performance updated to STF Processor, and allow parameters to be specified. Bug fix for create_patches_hdf5 verbose output reporting incorrect values create_patches_hdf5 verbose output now shows dimensions of last processed image pair

csbdeep/data/generate_hdf5.py
@@ -125,7 +125,7 @@ def create_patches_hdf5(
     ## summary
     if verbose:
 
-        n_patches_str = "{0}".format(n_patches_per_image) if n_patches_per_image is not None else "Undefined"
+        n_patches_str = "{0}".format(n_patches_per_image*n_images) if n_patches_per_image is not None else "Undefined"
         n_patches_per_image_str = "{0}".format(n_patches_per_image) if n_patches_per_image is not None else "Undefined"
         n_col_patches_per_image_str = "{0}".format(n_patches*collapse_multiplier) if n_patches_per_image is not None else "Undefined"
 
@@ -170,14 +170,18 @@ def create_patches_hdf5(
         X = f.create_dataset("X", datashape, maxshape=(datasize,)+tuple(out_patch_size), chunks=chunks, dtype='float32')
         Y = f.create_dataset("Y", datashape, maxshape=(datasize,)+tuple(out_patch_size), chunks=chunks, dtype='float32')        
 
+        tqdm_postfix = TQDMUpdatablePostFix("  ...")
         ## sample patches from each pair of transformed raw images
-        for i, (x,y,_axes,mask) in tqdm(enumerate(image_pairs),total=n_images,disable=(not verbose)):
+        for i, (x,y,_axes,mask) in tqdm(enumerate(image_pairs),total=n_images,disable=(not verbose),postfix=tqdm_postfix):
+            tqdm_postfix.set_postfix("  Last Image Shape {0}".format(str(x.shape)))
+
             if i >= n_images:
                 warnings.warn('more raw images (or transformations thereof) than expected, skipping excess images.')
                 break
             if i==0:
                 axes = axes_check_and_normalize(_axes,len(patch_size))
                 channel = axes_dict(axes)['C']
+
             # checks
             # len(axes) >= x.ndim or _raise(ValueError())
             axes == axes_check_and_normalize(_axes) or _raise(ValueError('not all images have the same axes.'))
@@ -186,16 +190,11 @@ def create_patches_hdf5(
             (channel is None or (isinstance(channel,int) and 0<=channel<x.ndim)) or _raise(ValueError())
             channel is None or patch_size[channel]==x.shape[channel] or _raise(ValueError('extracted patches must contain all channels.'))
 
-            _Y,_X = sample_patches_from_multiple_stacks((y,x), patch_size, n_patches_per_image, mask, patch_filter, overlap=overlap)
-            
-            n_patches_per_image_out = len(_X)
-            if (n_patches_per_image is not None) and (not overlap) and len(_X) != n_patches_per_image:
-                raise ValueError("Missmatched number of patches per image when no overlap")
-
+            _Y,_X = sample_patches_from_multiple_stacks((y,x), patch_size, n_patches_per_image, mask, patch_filter, overlap=overlap)            
             _Xn, _Yn = normalization(_X,_Y, x,y,mask,channel)
-            
-            n_patches_per_image_out = n_patches_per_image_out*collapse_multiplier
-            
+
+            n_patches_per_image_out = len(_X)*collapse_multiplier
+
             if n_patches_per_image is None:
                 X.resize(X.shape[0]+n_patches_per_image_out, axis=0)
                 Y.resize(Y.shape[0]+n_patches_per_image_out, axis=0)
@@ -207,12 +206,11 @@ def create_patches_hdf5(
                     X[-n_patches_per_image_out:] = _Xn
                     Y[-n_patches_per_image_out:] = _Yn
             else:
-                if collapse_channel:
-                    s = slice(i*n_patches_per_image*collapse_multiplier,(i+1)*n_patches_per_image*collapse_multiplier)
+                s = slice(i*n_patches_per_image_out,(i+1)*n_patches_per_image_out)
+                if collapse_channel:                    
                     X[s] = np.moveaxis(np.concatenate(_Xn, axis=channel),channel,0)[...,np.newaxis]
                     Y[s] = np.moveaxis(np.concatenate(_Yn, axis=channel),channel,0)[...,np.newaxis]
-                else:
-                    s = slice(i*n_patches_per_image,(i+1)*n_patches_per_image)
+                else:                    
                     X[s], Y[s] = _Xn, _Yn
             
             
@@ -220,7 +218,12 @@ def create_patches_hdf5(
         axes = 'S'+axes
         X.attrs['axes'] = axes
         Y.attrs['axes'] = axes
-
+        X.attrs['n_patches_per_image'] = n_patches_per_image_str
+        Y.attrs['n_patches_per_image'] = n_patches_per_image_str
+        X.attrs['n_raw_images'] = str(n_raw_images)
+        Y.attrs['n_raw_images'] = str(n_raw_images)
+        X.attrs['input_data'] = raw_data.description
+        Y.attrs['input_data'] = raw_data.description
     
     if shuffle:
         print('Shuffling HDF5 data via copy. This can take time for large datasets...')
@@ -341,3 +344,15 @@ class HDF5Data():
 
     def __len__(self):
         return len(self.sample_indx)
+
+# TQDM Helper
+class TQDMUpdatablePostFix():
+
+    def __init__(self, postfix):
+        self.postfix = postfix
+
+    def set_postfix(self, postfix):
+        self.postfix = postfix
+        
+    def __str__(self) -> str:
+        return self.postfix

csbdeep/data/prepare.py
@@ -176,9 +176,9 @@ class ReinhardNormalizer(Normalizer):
 
 class STFNormalizer(Normalizer):
 
-    def __init__(self, do_after=False):
+    def __init__(self, C=-4.0, B=0.185, do_after=False):
         self._do_after = do_after
-        self.normalizer = STFPreProcessor()
+        self.normalizer = STFPreProcessor(C=C,B=B)
 
     def before(self, x, axes):
         return self.normalizer.before(x,axes)
@@ -417,7 +417,7 @@ class ReinhardPreProcessor(PreProcessor):
     def do_after(self):
         return self._do_after
 
-
+import numexpr
 class STFPreProcessor(PreProcessor):
     """No Pre-processing.
 
@@ -436,34 +436,54 @@ class STFPreProcessor(PreProcessor):
     def mtf(m,x):
         # From: https://pixinsight.com/forum/index.php?threads/mathematically-exact-inverse-of-mtf.14912/
         
-        n = (m-1)*x
-        d = (2*m - 1)*x - m
-        return n/d
+        # This seems to be slower
+        # try:
+        #     import numexpr
+        #     mtf_result = numexpr.evaluate("((m - 1.0) * x) / ( (2.0 * m - 1.0) * x - m )",casting='no')
+        # except ImportError:
+        #     mtf_result =                   ((m - 1.0) * x) / ( (2.0 * m - 1.0) * x - m )
+        
+        # return mtf_result
+        
+        n = (m - 1.0) * x
+        d = (2.0 * m - 1.0) * x - m
+        return n / d        
 
     @staticmethod
-    def stf(data,C=-4,B=0.185,axis=[0,1]):
+    def stf(d,C=-4.0,B=0.185,axis=[0,1]):
         # From: JimmyTheChicken#2719 nightlyastronomy.com
         # C = -2.8
         # B = 0.25
         # c = min( max( 0, med( $T ) + C*mdev( $T ) ), 1 );
         # mtf( mtf( B, med( $T ) - c ), max( 0, ($T - c)/~c ) )
         
-        dimC = data.shape[-1]
-        med = np.median(data,axis=axis)
-        c = med + C*stats.median_abs_deviation(data,axis=axis,scale='normal')
-        c = np.max([np.zeros(dimC,), c],0)
-        c = np.min([c, np.ones(dimC,)],0)
+        med = np.median(d,axis=axis)
+        #mad = stats.median_absolute_deviation(d,axis=axis,scale=1.4826)
+        mad = stats.median_abs_deviation(d,axis=axis,scale='normal')
+        c = np.clip(med + C*mad,a_min=0,a_max=1)
         m = STFPreProcessor.mtf(B, med - c)
-        t = np.max([np.zeros_like(data), (data - c)/(1-c)],axis=0)
+        t = np.clip((d - c)/(1.0 - c),a_min=0,a_max=1)
         return STFPreProcessor.mtf(m, t)
 
-    def __init__(self, do_after=False, **kwargs):
+    @staticmethod
+    def mad(arr,axis):
+        """ Median Absolute Deviation: a "Robust" version of standard deviation.
+            Indices variabililty of the sample.
+            https://en.wikipedia.org/wiki/Median_absolute_deviation 
+        """
+        #arr = np.ma.array(arr).compressed() # should be faster to not use masked arrays.
+        med = np.median(arr, axis=axis)
+        return np.median(np.abs(arr - med),axis=axis)
+
+    def __init__(self, C=-4.0, B=0.185, do_after=False, **kwargs):
         self._do_after = do_after
         self.kwargs = kwargs
+        self.C = C
+        self.B = B
 
     def before(self, x, axes):
         axis = tuple(d for d,a in enumerate(axes) if a != 'C')
-        return STFPreProcessor.stf(x,axis=axis)
+        return STFPreProcessor.stf(x, C=self.C, B=self.B, axis=axis)
 
     def after(self, mean, scale, axes):
         self.do_after or _raise(ValueError())